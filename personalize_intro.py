0cd30c9ab23d3b3b3efabff007cb355408fab2e8
Ganesh-Ghumare <ganeshghumare85@gmail.com>
2025-12-24 10:15:57 +0530

    Add files via upload

---
+++ personalize_intro.py
@@ -0,0 +1,12 @@
+#personalize introduction program 
+#created by:Ganesh Ghumare
+name=input("what is your name? ")
+age=input("how old are you? ")
+education=input("what are you studying currently? ")
+city=input("where do you live? ")
+hobby=input("what is your fav hobby? ")
+
+print(f"\n welcome {name}!")
+print(f"so your are currently studying {education},")
+print(f"and you are {age} years old and love {hobby}.")
+print(f"so you live in {city}")
